9b5b21a6960ce1d89d3b18534d601815f9936c3f
Mangsa <1man0limbu@gmail.com>
2022-04-13 23:33:34 -0400

    Changes to PlayingCards

--- src/com/teamkoala/CLIView.java
+++ src/com/teamkoala/CLIView.java
@@ -35,8 +35,31 @@ public class CLIView extends View {
      */
     //Story 4
     @Override
-    public int displayTurnStart(int playerTurn, String playerHand) {
+    public void displayTurnStart(int playerTurn, String playerHand) {
         System.out.println(" Player " + playerTurn + "\'s turn. Player's hand: " + playerHand);
         System.out.println("Last discarded card: " + deck.drawDiscard());
+    }
+
+    // Story 5
+    @Override
+    public void drawCard() {
+        int temp;
+        System.out.println("Press 1 to draw a card from the draw pile or Press 2 to draw the last discarded card");
+        temp = input.nextInt();
+        if (temp == 1) {
+            // show card
+            System.out.println("The card drawn from the deck is: " + Deck.drawCard().PlayingCards.toString);
+            System.out.println("Press 1 to keep the card or Press 2 to discard it");
+            int temp2 = input.nextInt();
+            if (temp2 == 1) {
+                // replace card
+            }
+            else {
+                Deck.discard();
+            }
+        }
+        if (temp == 2) {
+            // replace card
+        }
     }
 }
