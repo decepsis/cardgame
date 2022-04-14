11a967f657a5ffdf2f6116647da3ae9eb17889fe
Mangsa <1man0limbu@gmail.com>
2022-04-13 20:27:12 -0400

    Changes to PlayingCards

--- src/com/teamkoala/PlayingCards.java
+++ src/com/teamkoala/PlayingCards.java
@@ -1,34 +1,138 @@
 package com.teamkoala;
 
+import java.util.Objects;
+
+/**
+ * Playing Card Object
+ * Version: 1.0
+ */
 public class PlayingCards {
-    /**
-     * Declaring variables to store information
-     */
-    private int suit, number;
+    private String suit; // Variable to hold the type of suit
+    private int number; // Variable to hold the numeral value of the card
+    private boolean faceDown; // Variable to see if the card is face down or not
 
     /**
-     * Constructor to initialize a card. Suits range from 1-4 and the number ranges from 1-13
+     * Constructor for PlayingCard
      * @param suit
      * @param number
+     * @param faceDown
      */
-    public PlayingCards(int suit, int number) {
+    public PlayingCards(String suit, int number, boolean faceDown) {
         this.suit = suit;
         this.number = number;
+        this.faceDown = faceDown;
     }
 
-    public boolean isRed(){
-        return true;
+    /**
+     * Method to check if the suit is red
+     * @return
+     */
+    public boolean isRed() {
+        if (Objects.equals(suit, "Diamond") || Objects.equals(suit, "Heart")) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    public boolean isBlack(){
+    /**
+     * Method to check if the suit is black
+     * @return
+     */
+    public boolean isBlack() {
+        if (Objects.equals(suit, "Diamond") || Objects.equals(suit, "Heart")) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    /**
+     * Getter for the card number
+     * @return
+     */
+    public int getCardNumber(){
+        return this.number;
+    }
+
+    /**
+     * Setter to set the card face up
+     */
+    public void setFaceUp() {
+        this.faceDown = false;
+    }
+
+    /**
+     * Setter to set the card face down
+     * @return
+     */
+    public void setFaceDown(){
+        this.faceDown = true;
+    }
+
+    /**
+     * Method to return if a card is face card or not
+     * @return
+     */
+    public boolean isFaceCard(){
+        if(number == 11 || number == 12 || number == 13){
+            return true;
+        }
+
         return false;
     }
 
-    public int getSuit() {
-        return suit;
+    /**
+     * toString method that returns the string of a card
+     * @return
+     */
+    public String toString() {
+        if (this.faceDown == true) {
+            return "Face down";
+        } else {
+            if (this.number == 1) {
+                return "Ace of " + this.suit;
+            }
+            if (this.number == 11) {
+                return "Jack of " + this.suit;
+            }
+            if (this.number == 12) {
+                return "Queen of " + this.suit;
+            }
+            if (this.number == 13) {
+                return "King of " + this.suit;
+            }
+
+            return this.number + " of " + this.suit;
+        }
     }
 
-    public int getNumber() {
-        return number;
+    /**
+     * Method that returns the score of the card
+     * @return
+     */
+    public int getCardScore(){
+        if(this.number == 1) {
+            return 1;
+        }
+        if(this.number == 2){
+            return -2;
+        }
+        if(this.number >= 3 && this.number <= 10){
+            return this.number;
+        }
+        if(this.number == 11 || this.number == 12){
+            return 10;
+        }
+        if(this.number == 13){
+            return 0;
+        }
+        if(this.number == 14 || this.number == 15){
+            return 15;
+        }
+
+        return 0;
     }
 }
